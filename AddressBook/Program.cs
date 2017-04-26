805d0ae8fdc508681513da1ef09168de427bef54
Curtis Schlak <curtis.schlak@theironyard.com>
2017-04-26 13:04:07 -0700

    Added a comment about how much I like this.

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,6 +7,8 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
+            // I LOVE THIS STUFF SO MUCH I NEVER WANT TO QUIT.
+            //    Love, Mark Twain
             string name = ConfigurationManager.AppSettings["ApplicationName"];
             Console.WriteLine("WELCOME TO:");
             Console.WriteLine(name);
